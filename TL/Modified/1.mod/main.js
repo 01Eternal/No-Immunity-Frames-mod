95b03edd48ad3f50d9f7df84d79ea0f733de81b9
TLEternal <109238149+01Eternal@users.noreply.github.com>
2024-12-06 09:08:15 -0300

    Added Original File

--- TL/Modified/1.mod/main.js
+++ TL/Modified/1.mod/main.js
@@ -1,1 +1,18 @@
+import { using } from './ModClasses.js';
 
+using('Terraria');
+using('Microsoft.Xna.Framework');
+
+// Disable Projectile I Frames
+Projectile.SetDefaults.hook((original, self, type) => {
+       original(self, type);
+        self.usesLocalNPCImmunity = true;
+        self.localNPCHitCooldown = 0;
+});
+
+// Disable Player I Frames
+Player.UpdateEquips.hook((original, self, i) => {
+    original(self, i)
+    
+    self.immuneTime = 0
+});
